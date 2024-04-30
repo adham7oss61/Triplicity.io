69d855355a9d87f4b95e83f45379a88768723425
adham7oss61 <168502147+adham7oss61@users.noreply.github.com>
2024-04-30 11:12:48 +0300

    Create confugiration.py

---
+++ app.py/confugiration.py
@@ -0,0 +1,4 @@
+import os
+
+api_url = os.getenv('/book''/profile''/review''/map')
+db_host = os.getenv('https://databases-auth.000webhost.com/index.php?route=/database/structure&db=id22079711_database')
