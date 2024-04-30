6cea8c15ddd8bc8e3d3640fc1641dcb72f8d698f
adham7oss61 <168502147+adham7oss61@users.noreply.github.com>
2024-04-30 11:02:16 +0300

    Create Social Media Login Functionality.py

---
+++ app.py/Api.py/Social Media Login Functionality.py
@@ -0,0 +1,4 @@
+from flask import Flask
+from flask_dance.contrib.google import make_google_blueprint
+from flask_dance.contrib.facebook import make_facebook_blueprint
+from flask_dance.contrib.instagram import make_instagram_blueprint
